64b9eec45b6d5acaf5e7be3e3ee8eb5b813e9a71
NahoKizawa <n.kizawa.629@gmail.com>
2021-12-17 22:39:49 +0900

    tutorial part4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My Gatsby Page",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-preact",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`, // 各フォルダに異なるnameオプションを指定することで特定のフォルダのみにフィルターをかけるGraphQLクエリを作成できる
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
